37deb2039ab8fa2d8973bd9cd44dd95038bd1ae6
Jordan Justice <jordan@concentrekgroup.com>
2020-12-12 17:24:05 -0500

    adds content for uses page

--- pages/uses.js
+++ pages/uses.js
@@ -1,9 +1,204 @@
+import Link from "next/link";
 import Layout from "../components/layout";
 
 export default function Uses() {
   return (
     <Layout>
       <h1>Uses</h1>
+      <p>Quick list of some of the things I enjoy using day to day.</p>
+      <h2>Equipment</h2>
+      <ul>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://everymac.com/systems/apple/iphone/specs/apple-iphone-12-pro-united-states-a2341-specs.html">
+              iPhone 12 Pro (Pacific Blue)
+            </Link>
+          </p>
+          <p className="leading-normal">
+            My first non-black/space gray iPhone. Consider it my attempt to
+            expand my life palette...
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://everymac.com/systems/apple/macbook_pro/specs/macbook-pro-core-i7-2.6-six-core-16-2019-scissor-specs.html">
+              Apple MacBook Pro 16-Inch "Core i7" 2.6 2019
+            </Link>
+          </p>
+          <p className="leading-normal">
+            Very quick and features a real escape key! Jumped up from a 13"
+            MacBook Pro, and I enjoy the screen real estate, but missing the
+            portability of the 13".
+          </p>
+        </li>
+      </ul>
+      <h2>Coding / Design</h2>
+      <ul>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://code.visualstudio.com/">VS Code</Link>
+          </p>
+          <p className="leading-normal">
+            Before this I used Atom, and before that it was Sublime. Since I
+            mainly develop React/Javascript ecosystem stuff, using VS Code is
+            top-notch experience.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://iterm2.com/">iTerm 2</Link>
+          </p>
+          <p className="leading-normal">
+            I've been trying to use VS Code's built in terminal and Git features
+            more, but it's a hard habit to break when iTerm is such a slick app.
+            Running ohmyzsh in it with a custom theme.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://developer.apple.com/fonts/">SF Mono</Link>
+          </p>
+          <p className="leading-normal">
+            I have yet to jump to a fancy programming font and find that SF Mono
+            is quite nice!
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://www.figma.com/">Figma</Link>
+          </p>
+          <p className="leading-normal">
+            My all time favorite design tool. So fast, and always updating with
+            useful features.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://www.github.com">GitHub</Link>
+          </p>
+          <p className="leading-normal">
+            Where I keep all my code. Another great company that is always
+            working to improve and ship helpful features that make our lives
+            easier. Also, did you know that{" "}
+            <Link href="https://kevinmarsh.com/">Kevin Marsh</Link> is GitHub
+            user #100!? 💯
+          </p>
+        </li>
+      </ul>
+      <h2>Other apps</h2>
+      <ul>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://sparkmailapp.com/">Spark Mail</Link>
+          </p>
+          <p className="leading-normal">
+            I really loved{" "}
+            <Link href="https://en.wikipedia.org/wiki/Mailbox_(application)">
+              Mailbox
+            </Link>
+            , remember that? After it was shutdown I moved on to Outlook which
+            had updated by acquiring something else, but on MacOS it was very
+            slow and then I discovered Spark Mail in 2018 and haven't looked
+            back. Its two most important features to me are the snoozing of
+            messages (inbox zero frequently/messages come back at relevant times
+            or as reminders) and being able to comment on email as a team
+            outside of the thread (no more forwarding to team members that can
+            help). I haven't tried those other two hipster email services yet,
+            because 1) I need my company email and 2) I don't get <i>that</i>{" "}
+            much email to make me think the prices are worth it. That's where
+            Spark Mail's monthly cost for team interaction is much more valuable
+            to me.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://brettterpstra.com/projects/nvalt/">nvALT</Link>
+          </p>
+          <p className="leading-normal">
+            Inspired by a friend, I <i>try</i> to keep a few daily notes.
+            Whether keeping tabs of to-dos as they come up, recording a log of
+            my process/steps during troubleshooting a bug, or noting restaurants
+            while traveling, nvALT makes it quick and easy to edit a text file
+            stored in Dropbox. Since I'm not doing this from my phone very
+            often, when I do I just use the Dropbox iOS app which allows you to
+            edit plain text files fairly easily. And yes, there are apps that
+            specialize in doing all the things I mentioned just now, but plain
+            text means it'll basically work forever.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://www.basecamp.com">Basecamp</Link>
+          </p>
+          <p className="leading-normal">
+            I'm pretty flexible in being in whatever project management tool a
+            team is using, but my favorite is Basecamp. I like their
+            message/comment thread feature way more than messy email threads,
+            adore their tidy to-do management, and often use the email
+            forwarding feature to centralize conversations and get us back on
+            track. I've also found that people who don't use it often appreciate
+            the fact they get emails that they can respond to from the system
+            without ever having to log into the app. And all for a flat rate
+            $99/mo. It might seem like a lot for a team of 3 or 4, but for a
+            team of 10+ it's a steal.
+          </p>
+        </li>
+      </ul>
+      <h2>Kitchen stuff</h2>
+      <ul>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://www.lecreuset.com/round-dutch-oven/LS2501.html">
+              Le Creuset Dutch Oven
+            </Link>
+          </p>
+          <p className="leading-normal">
+            We've had a 5.5 quart round dutch oven from Le Creuset for about 10
+            years now, and use it almost daily. We also were gifted a gigantic 9
+            quart one more recently from someone combining kitchens with their
+            significant other. They're just simple, solid pieces of cookware
+            with a billion uses. We use them for soups, searing, rice, and
+            they're perfect at trapping in steam when making a rustic loaf of
+            bread. I'm not too familiar with other brands, but I'm sure they
+            good as well. I'd only suggest making sure the one you get has a
+            metal knob on top so that it can withstand higher oven temps.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://www.all-clad.com/cookware/collections/d3-stainless.html">
+              All-Clad Cookware
+            </Link>
+          </p>
+          <p className="leading-normal">
+            After our initial "non-stick" cookware from our wedding registry
+            wore out (and was potentially toxic?), we began investing in
+            All-Clad pans. You don't need the non-stick for most things and the
+            3-ply structure of these pans distribute heat much better. Like the
+            Le Creuset, I think these will pretty much last for life. We've only
+            ever got the D3 series, but I've heard good things about the copper
+            core line, and may invest in one or two of those eventually.
+          </p>
+        </li>
+        <li>
+          <p className="leading-normal">
+            <Link href="https://misen.com/products/serrated-knife">
+              Misen Serrated Knife (bread knife)
+            </Link>
+          </p>
+          <p className="leading-normal">
+            Our most recent kitchen addition. After baking more loaves I was
+            looking to upgrade to a longer serrated knife that could handle a
+            thick crusty bread a bit better. Originally I learned about Misen
+            because of their chef's knife from Kenji Lopez-Alt. Seeing how nice
+            this knife is, I'm interested in trying our the chef's knife soon.
+            Bonus: They offer free sharpening if you pay for shipping and
+            handling.
+          </p>
+        </li>
+      </ul>
+      <h2>Bike stuff</h2>
+      <p className="italic text-gray-500">Coming soon!</p>
     </Layout>
   );
 }
